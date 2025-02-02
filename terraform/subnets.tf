b9cc2fd70665d5bf22b5ec6446f2b40b544ea2c0
Wanen Gao <wanengao@Wanens-MBP.lan>
2025-02-02 10:04:16 -0800

    added subnets

---
+++ terraform/subnets.tf
@@ -0,0 +1,43 @@
+resource "aws_subnet" "public_1" {
+    vpc_id = aws_vpc.vpc.vpc_id
+    cidr_block = var.public_subnets[0]
+    availability_zone = var.availability_zone[0]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "${var.app_name}-public-1"
+    }
+}
+
+resource "aws_subnet" "private_2" {
+    vpc_id = aws_vpc.vpc.vpc_id
+    cidr_block = var.public_subnets[1]
+    availability_zone = var.availability_zone[1]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "${var.app_name}-public-2"
+    }
+}
+
+resource "aws_subnet" "private_1" {
+    vpc_id = aws_vpc.vpc.vpc_id
+    cidr_block = var.private_subnets[0]
+    availability_zone = var.availability_zone[0]
+    map_private_ip_on_launch = true
+
+    tags = {
+        Name = "${var.app_name}-private-1"
+    }
+}
+
+resource "aws_subnet" "private_2" {
+    vpc_id = aws_vpc.vpc.vpc_id
+    cidr_block = var.private_subnets[1]
+    availability_zone = var.availability_zone[1]
+    map_private_ip_on_launch = true
+
+    tags = {
+        Name = "${var.app_name}-private-2"
+    }
+}
